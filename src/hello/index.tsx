856ad8c997147b9b109e840f9a6fc6ddae6a70b6
kosaku kurino <kousaku.maron@gmail.com>
2020-07-29 12:31:13 +0900

    feat: add styled

--- src/hello/index.tsx
+++ src/hello/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Hello = () => {
-  return <p>Hello</p>
+  return <Heading>Hello</Heading>
 }
+
+const Heading = styled.p`
+  color: red;
+`
 
 export default Hello
